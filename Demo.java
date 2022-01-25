d28b5b201c4391c09e7af0cbcddf2b7aa7aa2ceb
Mahalakshmi Mothilal <mahalakshmim@stg.com>
2022-01-25 11:46:44 +0530

    save

--- Demo.java
+++ Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	public static void main(String[] args)
 	{
 		System.out.println("executing demo project ...");
+System.out.println("demo");
+System.out.println("juju");
 	}
 
 }
